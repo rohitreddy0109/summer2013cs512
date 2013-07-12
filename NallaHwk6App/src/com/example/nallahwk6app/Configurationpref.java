a25be2c7cbc08b388575e38cc67d4504210ad565
rohit <rohit@192.168.1.146>
2013-07-12 05:24:07 -0400

    androidcommit

---
+++ NallaHwk6App/src/com/example/nallahwk6app/Configurationpref.java
@@ -0,0 +1,17 @@
+package com.example.nallahwk6app;
+
+
+
+
+	
+	import android.app.Activity;
+	import android.content.Intent;
+	import android.os.Bundle;
+	import android.view.View;
+	import android.widget.Button;
+	import android.widget.EditText;
+
+
+	public class Configurationpref extends Activity {
+		
+		}
